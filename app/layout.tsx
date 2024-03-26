6100509c26fbfbb50ed0d48d186ef7274555ec41
misterh100 <handsomenyathi1@gmail.com>
2024-03-26 11:49:33 +0200

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600", "900"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={poppins.className}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,305 @@
-import Image from "next/image";
+"use client";
+import { useTheme } from "next-themes";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { PlusCircleIcon, MoonIcon, SunIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 
+interface IActivity {
+  _id: string;
+  title: string;
+  description: string;
+  start_date: Date;
+  end_date: Date;
+  duration: string;
+}
 export default function Home() {
+  const { setTheme, theme } = useTheme();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [completeActivities, setCompleteActivities] = useState<IActivity[]>([]);
+  const [loading, setLoading] = useState(false);
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const getActivities = async () => {
+    setLoading(true);
+    const response = await fetch(
+      "https://nodeserver-v2.onrender.com/api/activities",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    setActivities(data.filter((item: IActivity) => item.end_date == null));
+    setCompleteActivities(
+      data.filter((item: IActivity) => item.end_date != null)
+    );
+    setLoading(false);
+  };
+
+  const startActivity = async () => {
+    setLoading(true);
+    await fetch("https://nodeserver-v2.onrender.com/api/activity", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: title, description: description }),
+    }).then(() => {
+      getActivities();
+    });
+    setTitle("");
+    setDescription("");
+    setLoading(false);
+  };
+
+  const endActivity = async (id: string) => {
+    setLoading(true);
+    await fetch(`https://nodeserver-v2.onrender.com/api/activity/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      getActivities();
+    });
+    setLoading(false);
+  };
+  const calculateDuration = (end: Date, start: Date) => {
+    const startDate = new Date(start).getTime();
+    const endDate = new Date(end).getTime();
+    const timeDifferenceMS = endDate - startDate;
+    const seconds = Math.floor(timeDifferenceMS / 1000);
+    const minutes = Math.floor(
+      (timeDifferenceMS % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const hours = Math.floor(timeDifferenceMS / (1000 * 60 * 60));
+    const days = Math.floor(timeDifferenceMS / (1000 * 60 * 60 * 24));
+    if (hours > 23) {
+      return `${days} days`;
+    }
+    if (hours < 1) {
+      return `${minutes} minutes`;
+    }
+    return `${hours} hours ${minutes} minutes`;
+  };
+
+  const formatTime = (date: Date) => {
+    const offsetMinutes = new Date(date).getTimezoneOffset();
+    const milliseconds = new Date(date).getTime();
+    const localTime = milliseconds - offsetMinutes * 60 * 1000;
+    const seconds = Math.floor((localTime / 1000) % 60);
+    const minutes = Math.floor((localTime / 1000 / 60) % 60);
+    const hours = Math.floor((localTime / 1000 / 60 / 60) % 24);
+
+    return `${hours < 10 ? "0" : ""}${hours}:${
+      minutes < 10 ? "0" : ""
+    }${minutes} ${hours >= 12 ? "pm" : "am"}`;
+  };
+
+  const formatDate = (date: Date) => {
+    return ` ${days[date.getDay()]} ${date.getDate()} ${
+      months[date.getMonth()]
+    }`;
+  };
+
+  useEffect(() => {
+    getActivities();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="flex min-h-screen flex-col items-center justify-between px-4 md:px-10">
+      <section className="relative w-full md:w-[600px] min-h-screen border border-border px-4">
+        {loading && <div className="loaderBar"></div>}
+        <Card className="border-none">
+          <CardHeader className="flex-row justify-between px-0">
+            <div>
+              <CardTitle>Productivity Tracker</CardTitle>
+              <CardDescription>
+                Today:{formatDate(new Date(Date.now()))}
+              </CardDescription>
+            </div>
+            <div>
+              <Button
+                onClick={() => setTheme(theme == "light" ? "dark" : "light")}
+                variant="outline"
+              >
+                {theme == "light" ? <SunIcon /> : <MoonIcon />}
+              </Button>
+            </div>
+          </CardHeader>
+        </Card>
+        <Card className="border-none shadow-none px-0">
+          <CardContent className="px-0">
+            <CardTitle className="text-base">Add new activity</CardTitle>
+            <div className="flex gap-2 mt-2">
+              <Input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Activity Title"
+                maxLength={100}
+                className="rounded-md"
+              />
+              <Dialog>
+                <DialogTrigger
+                  className={`${buttonVariants({
+                    variant: "outline",
+                  })} rounded-full `}
+                >
+                  <PlusCircleIcon />
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Create Activity</DialogTitle>
+                  </DialogHeader>
+                  <Input
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Activity Title"
+                    maxLength={100}
+                    className="rounded-full"
+                  />
+                  <Input
+                    type="text"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    placeholder="Activity Description"
+                    maxLength={200}
+                    className="h-20"
+                  />
+                  <DialogFooter>
+                    <DialogClose
+                      className={`${buttonVariants({
+                        variant: "default",
+                      })} rounded-full text-white`}
+                      onClick={startActivity}
+                    >
+                      Start activity
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </div>
+          </CardContent>
+        </Card>
+        <p className="w-full text-sm text-center text-muted-foreground my-2">
+          Recent
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="flex flex-col gap-4">
+          {activities.map((activity) => (
+            <Card key={activity._id}>
+              <CardHeader className="flex-row justify-between">
+                <div>
+                  <CardTitle>{activity.title}</CardTitle>
+                  <CardDescription>{activity.description}</CardDescription>
+                </div>
+                <div>
+                  <span className="text-xs text-muted-foreground">
+                    {formatDate(new Date(activity.start_date))}
+                  </span>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <span className="inline-block w-1/2">
+                  Started:
+                  <span className="pl-2">
+                    {formatTime(activity.start_date)}
+                  </span>
+                </span>
+                <span className="inline-block w-1/2 text-right">
+                  In progress...
+                </span>
+              </CardContent>
+              <CardFooter>
+                <div>
+                  <Button
+                    onClick={() => {
+                      endActivity(activity._id);
+                    }}
+                    className="rounded-full"
+                    variant="destructive"
+                  >
+                    End activity
+                  </Button>
+                </div>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <Separator className="my-4" />
+        <p className="w-full text-sm text-center text-muted-foreground my-2">
+          History
+        </p>
+        <div className="flex flex-col gap-4">
+          {completeActivities.map((item) => (
+            <Card key={item._id}>
+              <CardHeader className="flex-row justify-between">
+                <div>
+                  <CardTitle>{item.title}</CardTitle>
+                  <CardDescription>{item.description}</CardDescription>
+                </div>
+                <div>
+                  <span className="text-xs text-muted-foreground">
+                    {formatDate(new Date(item.start_date))}
+                  </span>
+                </div>
+              </CardHeader>
+              <CardFooter>
+                Duration:
+                <span className="pl-2">
+                  {calculateDuration(item.end_date, item.start_date)}
+                </span>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
